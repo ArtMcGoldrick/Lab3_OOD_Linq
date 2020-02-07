bf0342478a5a42b330ad3f01d49cb0b7e937d3ab
S00186896 <S00186896@LAB-D2036-04>
2020-02-07 10:01:46 +0000

    Add project files.

---
+++ Lab3_OOD_Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Lab3_OOD_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 5, 3, 6, 11, 2, 15, 21, 13, 12, 10 };
+
+            //Linq Query
+            //var ouptut = from number in numbers
+            //             where number > 5
+            //             orderby number descending
+            //             select number;
+
+            var output = numbers
+                .Where(num => num > 5)
+                .OrderByDescending(n => n);
+
+            foreach (int number  in output)
+            {
+                Console.WriteLine(number.ToString());
+            }
+        }
+    }
+}
---
+++ Lab3_OOD_Linq_Q2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab3_OOD_Linq_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuerySyntax();
+
+
+        }
+
+        private static void QuerySyntax()
+        {
+            var files = new DirectoryInfo("C:\\Windows").GetFiles();
+
+            var query = from item in files
+                        where item.Length > 10000
+                        orderby item.Length, item.Name
+                        select new MyFileInfo
+                        {
+                            Name = item.Name,
+                            Length = item.Length,
+                            CreationTime = item.CreationTime
+                        };
+
+            foreach (var item in query)
+            {
+                //Console.WriteLine($"{item.Name} - {item.Length}");
+                Console.WriteLine(item);
+            }
+        }
+
+
+        private static void LambdaSyntax()
+        {
+
+        }
+
+        public class MyFileInfo
+        {
+            public string Name { get; set; }
+            public long Length { get; set; }
+            public DateTime CreationTime { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name} - {Length} - {CreationTime.ToShortDateString()}";
+            }
+        }
+    }
+}
+
+
+
